2e077174b5238d161e39e39a5835a579c2775f3b
timuruski <timuruski@gmail.com>
2010-07-08 08:28:26 -0600

    Reorganized inspector display code. In the middle of adding settings to preserve inspector state.

--- CacheDebugger.safariextension/inject.js
+++ CacheDebugger.safariextension/inject.js
@@ -11,11 +11,11 @@ function handleMessage (event) {
         case 'updateToolbarItem':
             updateToolbarItem();
             break;
-        case 'toggleManifestInspector':
-            toggleManifestInspector();
+        case 'toggleCacheInspector':
+            toggleCacheInspector();
             break;
-        case 'loadInspectorTemplate':
-            onLoadInspectorTemplate(event.message);
+        case 'fetchInspectorTemplate':
+            onFetchInspectorTemplate(event.message);
             break;
     }
 }
@@ -99,88 +99,172 @@ applicationCache.addEventListener('updateready', onUpdateReady, false);
 // DOMApplicationCache.UPDATEREADY = 4
 // DOMApplicationCache.OBSOLETE    = 5
 
+
 // Display the manifest inspector
 // ==============================
-var inspectorTemplate;
-function loadInspectorTemplate () {
-	safari.self.tab.dispatchMessage('loadInspectorTemplate');
+var cacheInspector;
+function fetchInspectorTemplate () {
+	safari.self.tab.dispatchMessage('fetchInspectorTemplate');
 }
-function onLoadInspectorTemplate (message) {
-    // console.log(message);
-    var inspector = document.createElement('div');
-    inspector.innerHTML = message;
-    document.body.appendChild(inspector.children[0]);
+function onFetchInspectorTemplate (message) {
+    var stub = document.createElement('div');
+    stub.innerHTML = message;
+	cacheInspector = stub.children[0];
+	document.body.appendChild(cacheInspector);
+    // cacheInspector = document.getElementById('cacheInspector');
+	// cacheInspector_closeBtn = cacheInspector_closeBtn.getElementById('cacheInspector_closeBtn');
+	// cacheInspector_closeBtn.addEventListener('click', hideCacheInspector, false);
+    // cacheInspector.addEventListener('mousedown', startDragCacheInspector, false);
+	cacheInspector_status = document.getElementById('cacheInspector_status');
+	cacheInspector_manifestURL = document.getElementById('cacheInspector_manifestURL');
+	cacheInspector_manifestHeaders = document.getElementById('cacheInspector_manifestHeaders');
+	cacheInspector_manifestContent = document.getElementById('cacheInspector_manifestContent');
+	// cacheInspector_manifestRaw.addEventListener('mousedown', startDragCacheInspector, false);
+	
+	// Use an invisible setting to store these states.
+	toggleManifestHeaders(safari.extension.manifestHeadersOpen);
+	toggleManifestContent(safari.extension.manifestContentOpen);
 }
-loadInspectorTemplate();
+// function showCacheInspector () {
+//     cacheInspector.style.display = 'block';
+// }
+// function hideCacheInspector () {
+//     cacheInspector.style.display = 'none';
+// }
+// function toggleCacheInspector () {
+//     if(cacheInspector) {
+//         if(cacheInspector.style) showCacheInspector();
+//      else hideCacheInspector();
+//     }
+// }
+fetchInspectorTemplate();
 
-var manifestInspector, manifestInspector_body, manifestInspector_closeBtn;
-function showManifestInspector () {
-    // Download manifest inspector
-    // This should probably be moved into the global HTML page
-    var request = new XMLHttpRequest();
-    request.onreadystatechange = function () {
-        if(request.readyState === 4 /*&& request.status == 200*/) {
-            onLoadCacheManifest(request.responseText);
-        }
-    }
-    request.open('GET', manifestURL);
-    request.send(null);
-}
-function hideManifestInspector () {
-    var body = document.getElementsByTagName('body')[0];
-    body.removeChild(manifestInspector);
-}
-function toggleManifestInspector () {
-    document.getElementById('manifestInspector') ? hideManifestInspector() : showManifestInspector();
-}
 
-function onLoadCacheManifest (text) {
-    var body = document.getElementsByTagName('body')[0], 
-        html = '<p id="manifestInspector_closeBtn"><span>x</span></p>' + 
-               '<p id="manifestInspector_title">Manifest</p>' + 
-               '<pre id="manifestInspector_body">' + text + '</pre>';
-    // console.log(manifestInspector);
-    if(manifestInspector == null) {
-        manifestInspector = document.createElement('div');
-        manifestInspector.id = 'manifestInspector';
-        manifestInspector.innerHTML = html;
-    }
-    body.appendChild(manifestInspector);
-    manifestInspector_body = document.getElementById('manifestInspector_body');
-    manifestInspector_closeBtn = document.getElementById('manifestInspector_closeBtn');
-    // manifestInspector.addEventListener('mousedown', startDragManifestInspector);
-    manifestInspector_closeBtn.addEventListener('click', hideManifestInspector, false);
-    manifestInspector.addEventListener('mousedown', startDragManifestInspector, false);
-}
+var cacheInspector, 
+	cacheInspector_closeBtn, 
+	cacheInspector_status, 
+	cacheInspector_manifestURL, 
+	cacheInspector_manifestHeaders, 
+	cacheInspector_manifestContent;
 
-function startDragManifestInspector (event) {
-    if(event.target === manifestInspector_body) return;
-    // Initial click position
-    var startX = event.clientX, 
-        startY = event.clientY, 
-        originX = manifestInspector.offsetLeft, 
-        originY = manifestInspector.offsetTop;
-    
-    event.stopPropagation();
-    event.preventDefault();
-    window.addEventListener('mousemove', onMouseMove, true);
-    window.addEventListener('mouseup', onMouseUp, true);
-    
-    function onMouseMove (event) {
-        var deltaX = startX - originX, 
-            deltaY = startY - originY, 
-            mouseX = event.clientX, // + window.scrollX,
-            mouseY = event.clientY, // + window.scrollY;
-            posX = (mouseX - deltaX), 
-            posY = (mouseY - deltaY);
-        // Prevent inspector from being dragged off the screen.
-        // Tries to mimic window behavior in OS X.
-        if((mouseX >= 0)) manifestInspector.style.left = posX + 'px';
-        manifestInspector.style.top = ( (posY >= 0) ? posY : 0 ) + 'px';
-    }
-    function onMouseUp (event) {
-        window.removeEventListener('mousemove', onMouseMove, true);
-        window.removeEventListener('mouseup', onMouseUp, true);
-    }
+// Startup
+function init (event) {
+	cacheInspector = document.getElementById('cacheInspector');
+	cacheInspector.addEventListener('mousedown', startDragCacheInspector, false);
+	cacheInspector_closeBtn = document.getElementById('cacheInspector_closeBtn');
+	cacheInspector_closeBtn.addEventListener('click', hideCacheInspector, false);
+	cacheInspector_status = document.getElementById('cacheInspector_status');
+	cacheInspector_manifestURL = document.getElementById('cacheInspector_manifestURL');
+	cacheInspector_manifestHeaders = document.getElementById('cacheInspector_manifestHeaders');
+	cacheInspector_manifestContent = document.getElementById('cacheInspector_manifestContent');
+	// cacheInspector_manifestRaw.addEventListener('mousedown', startDragCacheInspector, false);
+	
+	// hideCacheInspector();
+	
+	showManifestSection('cacheInspector_manifestHeaders');
+	hideManifestSection('cacheInspector_manifestContent');
+	// showDetailedManifest();
+	// showFormattedManifest();
+	// showRawManifest();
+	
+	// loadManifest('../mobile-me.manifest');
+	// loadManifest('../html5demo.manifest');
+	
+	// cacheInspector_status.getElementsByClassName('value');
+	// cacheInspector_status.getElementsByClassName('statusCode');
 }
 
+
+// Manifest loading and parsing
+function loadManifestContent (url) {
+	cacheInspector_manifestURL.getElementsByClassName('value')[0].innerText = url;
+	var request = new XMLHttpRequest();
+	var onManifestLoaded = function (event) {
+		// console.log(request);
+		if(request.readyState === 4) {
+			parseManifest(request);
+			setManifestHttpStatus(request.status);
+		}
+	}
+	request.addEventListener('readystatechange', onManifestLoaded);
+	request.open('GET', url);
+	request.send();
+}
+function parseManifest (request) {
+	cacheInspector_manifestHeaders.getElementsByTagName('pre')[0].innerText = request.getAllResponseHeaders();
+	cacheInspector_manifestContent.getElementsByTagName('pre')[0].innerText = request.responseText;
+	// showCacheInspector();
+}
+function setManifestHttpStatus (status) {
+	var badge = cacheInspector_manifestURL.getElementsByClassName('httpCode')[0];
+	var c = '';
+	if(status >= 200 && status < 300) c = 'successful';
+	if(status >= 300 && status < 400) c = 'redirect';
+	if(status >= 400 && status < 500) c = 'clientError';
+	if(status >= 500) c = 'serverError';
+	badge.innerText = status;
+	badge.className = 'httpCode ' + c;
+	// cacheInspector_manifestURL.getElementsByClassName('httpCode')[0].innerText = status;
+}
+
+// Disclosure sections
+function isManifestHeadersOpen () { return cacheInspector_manifestHeaders.className === 'open'; }
+function toggleManifestHeaders (open) {
+    if(typeof open === 'undefined') { open = !isManifestHeadersOpen() }
+    cacheInspector_manifestHeaders.className = open ? 'open' : 'closed';
+    safari.extension.settings.manifestHeadersOpen = open;
+}
+function isManifestContentOpen () { return cacheInspector_manifestContent.className === 'open'; }
+function toggleManifestContent (open) {
+    if(typeof open === 'undefined') { open = !isManifestContentOpen() }
+    cacheInspector_manifestContent.className = open ? 'open' : 'closed';
+    safari.extension.settings.manifestContentOpen = open;
+}
+
+
+// Manifest inspector
+function toggleCacheInspector () {
+    // document.getElementById('cacheInspector') ? hideCacheInspector() : showCacheInspector();
+    // cacheInspector.style.opacity = (cacheInspector.style.opacity === 0) ? 1 : 0;
+    cacheInspector.style.display = (cacheInspector.style.display === 'block') ? 'none' : 'block';
+}
+function showCacheInspector () {
+	// document.body.appendChild(cacheInspector);
+    cacheInspector.style.display = 'block';
+    // cacheInspector.style.opacity = 1;
+}
+function hideCacheInspector () {
+	// document.body.removeChild(cacheInspector);
+    cacheInspector.style.display = 'none';
+    // cacheInspector.style.opacity = 0;
+}
+
+// Drag functionality
+function startDragCacheInspector (event) {
+	if(event.target === cacheInspector_manifestHeaders || event.target === cacheInspector_manifestContent) return;
+	// Initial click position
+	var startX = event.clientX, 
+		startY = event.clientY, 
+		originX = cacheInspector.offsetLeft, 
+		originY = cacheInspector.offsetTop;
+
+	event.stopPropagation();
+	event.preventDefault();
+	window.addEventListener('mousemove', onMouseMove, true);
+	window.addEventListener('mouseup', onMouseUp, true);
+
+	function onMouseMove (event) {
+		// Prevent inspector from being dragged off the screen.
+		if(event.clientX < 0 || event.clientY < 0) return;
+		var deltaX = startX - originX, 
+			deltaY = startY - originY, 
+			mouseX = event.clientX // + window.scrollX,
+			mouseY = event.clientY // + window.scrollY;
+		cacheInspector.style.left = (mouseX - deltaX) + 'px';
+		cacheInspector.style.top = (mouseY - deltaY) + 'px';
+	}
+	function onMouseUp (event) {
+		window.removeEventListener('mousemove', onMouseMove, true);
+		window.removeEventListener('mouseup', onMouseUp, true);
+	}
+}
